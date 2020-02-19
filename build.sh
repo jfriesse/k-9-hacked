fb65dacfb3c5af3e8ffc7524fb1afb6451b9b79d
Jan Friesse <jfriesse@gmail.com>
2020-02-19 18:00:54 +0100

    Add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -xeo pipefail
+
+export ANDROID_HOME=$HOME/android-sdk
+export PATH=$ANDROID_HOME/platform-tools:$PATH
+export PATH=$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$PATH
+
+sdkmanager 'build-tools;27.0.3' 'platforms;android-27'
+
+./gradlew assembleDebug
+./gradlew assembleRelease
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ~/.android/debug.keystore k9mail/build/outputs/apk/release/k9mail-release-unsigned.apk androiddebugkey -storepass android
